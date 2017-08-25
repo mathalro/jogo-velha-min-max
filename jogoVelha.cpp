5ab56ec6f06dd5b9a7d6ea862f5a80ac46a1cffd
root <root@localhost.localdomain>
2017-08-25 08:45:51 -0300

    Gerando estado

--- jogoVelha.cpp
+++ jogoVelha.cpp
@@ -4,13 +4,14 @@
 */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 const int sizeTab = 3;
 const int contGanhaBola = 30, contGanhaX = 3;
 const int ganhaBola = 1, ganhaX = 2;
-const int bola = 1, xis = 2;
+const int bola = 10, xis = 1;
 
 int tab[sizeTab][sizeTab] = {{1, 1, 10},
 														 {10, 1, 1},
@@ -19,7 +20,7 @@ int tab[sizeTab][sizeTab] = {{1, 1, 10},
 /*
 	Verifica se nao existem mais casas para jogar
 */
-bool impossivel() {
+bool impossivelJogar() {
 	int cont;
 	for (int i = 0; i < sizeTab; i++) {
 		for (int j = 0; j < sizeTab; j++) {
@@ -48,37 +49,64 @@ int verificaGanhador() {
 	// verifica diagonais
 	int contPrincipal = 0, contSecundaria = 0;
 	for (int i = 0; i < sizeTab; i++) {
-		cout << tab[i][sizeTab-i-1] << ' ';
 		contPrincipal += tab[i][i];
 		contSecundaria += tab[i][sizeTab-i-1];
-		cout << contSecundaria << endl;
 	}
-
 	if (contPrincipal == contGanhaBola || contSecundaria == contGanhaBola) return ganhaBola;
 	if (contPrincipal == contGanhaX || contSecundaria == contGanhaX) return ganhaX;
 
 	return 0;
 }
 
-void constroiTree(int v[sizeT][], int jogador) {
+int trocaJogador(int jogador) {
+	if (jogador == xis) return bola;
+	return xis;
+}
+
+void print() {
+	puts("");
+	for (int i = 0; i < sizeTab; i++) {
+		for (int j = 0; j < sizeTab; j++) {
+			if (tab[i][j] == bola) {
+				printf("o ");
+			} else {
+				printf("x ");
+			}
+		}puts("");
+	}
+}
+
+int constroiTree(int jogador) {
+	
+	print();	
+	
 	int estado = verificaGanhador();
-	if (estado == ganhaBola) return 1;
-	if (estado == ganhaX) return -1;
+	if (estado == ganhaBola) {
+		cout << "O ganhou!!" << endl;
+		return 1;
+	}
+	if (estado == ganhaX) {
+		cout << "X ganhou!!" << endl;
+		return -1;
+	}
 	if (impossivelJogar()) return 0;
 
 	for (int i = 0; i < sizeTab; i++) {
 		for (int j = 0; j < sizeTab; j++) {
 			if (!tab[i][j]) {
-				
+				tab[i][j] = jogador;
+				constroiTree(trocaJogador(jogador));
+				tab[i][j] = 0;
 			}
 		}
 	}
-
 }
 
 int main () {
 	
-	
+
+	memset(tab, 0, sizeof tab);
+	constroiTree(xis);
 
 	return 0;
 }
